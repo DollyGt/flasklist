fd5bfbac4f6c78e56739ec95615e0231e899df34
DollyG <dollsgoitse@yahoo.com>
2018-05-30 17:23:59 +0000

    revised py file

--- flasklist.py
+++ flasklist.py
@@ -1,25 +1,40 @@
 import os
-from flask import Flask, request, render_template, redirect
+from flask import Flask, redirect, render_template, request
 from random import choice
+import json
 
 app = Flask(__name__)
 
-welcome_messages = ["Hi", "Hello", "Bonjour", "Ciao","Hola","Dumela"]
 
-items = [
-    "Buy Computer",
-    "Learn Python"
-    ]
+welcome_messages = ["Hi", "Hello", "Bonjour", "Ciao", "Hola", "Annyeonghaseyo"]
+
+def load_list(filename):
+    with open(filename, "r") as f:
+        data = f.read()
+        items = json.loads(data)
+    return items
+    
+def save_list(filename, items):
+    with open(filename, "w") as f:
+        data = json.dumps(items)
+        f.write(data)
+    
+
 
 @app.route("/")
 def get_index():
+    items = load_list("data/list.json")    
     welcome = choice(welcome_messages)
-    return render_template("index.html", tasks=items)
+    return render_template('index.html', msg=welcome, tasks=items)
 
-@app.route("/new_task", methods=["POST"])
-def create_task():
-    task= request.args["task_to_do"]
+@app.route("/new_task", methods=['POST'])
+def create_a_task():
+    task = request.form['task_to_do']
+    
+    items = load_list("data/list.json")
     items.append(task)
+    save_list("data/list.json", items)
+    
     return redirect("/")
 
 
